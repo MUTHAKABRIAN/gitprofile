ee1668c44114776cd939ae8928d2a73ee0da1800
MUTHAKABRIAN <muthakabrian@gmail.com>
2020-06-22 00:22:11 +0300

    add date count pipe

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  myApi:"7d12707fbc51eddb823ed8f3b7a0bbbcda1f3fec"
 };
 
 /*
